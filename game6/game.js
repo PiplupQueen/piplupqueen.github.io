96f4b8ea94dff10cf9c5c7f8ee84b2c8c99c61db
Piplup Queen <piplupqueen@gmail.com>
2015-11-05 10:41:18 +0800

    game6

---
+++ game6/game.js
@@ -0,0 +1,10 @@
+//javascript
+
+$(document).ready(function(){
+    $("start").click(function(){
+        $("p").slideToggle();
+    });
+$("#start").click(function() {
+  $("#start").css("display", "none");
+  }
+});
