0d8a0af2113bce6a8cfcd59bdd43f70fd2a4a86e
Piplup Queen <piplupqueen@gmail.com>
2015-11-05 15:40:21 +0800

    game6

--- game6/game.js
+++ game6/game.js
@@ -3,7 +3,16 @@ $(document).ready(function() {
   $("#start").click(function() {
     $("#start").hide();
     $("#memegame").css("background-image", "url('http://piplupqueen.github.io/borderlands.jpg')");
+    $("#shrek").hide();
+    $("#clickme").hide();
+    $("#fire").hide();
+    $(".doge").show();
+    $(".crate").show();
   });
 
+  $(".crate").click(function() {
+    $(".crate").hide();
 
 });
+
+ });
