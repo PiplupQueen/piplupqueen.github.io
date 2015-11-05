5005192733eae57bf14ac94f3d1e17dce87ad997
Piplup Queen <piplupqueen@gmail.com>
2015-11-05 11:38:17 +0800

    pic

--- game6/game.js
+++ game6/game.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
   $("#start").click(function() {
     $("#start").hide();
+    $("#memegame").css("background-image", "url('http://piplupqueen.github.io/borderlands.jpg')");
   });
+
 
 });
