4a7540227c081323738b5b2f2f38850a32da5260
Piplup Queen <piplupqueen@gmail.com>
2015-11-06 15:24:28 +0800

    GAME PLS HELP

--- game6/game.js
+++ game6/game.js
@@ -8,11 +8,579 @@ $(document).ready(function() {
     $("#fire").hide();
     $(".doge").show();
     $(".crate").show();
+
+  //  var doge = '2', troll = '0';
+  
+ // $(".doge").click(function() {
+
+  //  troll = '1';
+
+   // if(doge==troll) {
+   //   $(".doge").hide();
+   //   $(".troll").show();
+  //  }
+  //  else if(doge != troll) {
+
+ //   }
+
+  //  else {
+  //    $("body").html("<p>Cheating!</p>");
+ //   }
+    
+//  });
+
+//   $(".doge2").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge2").hide();
+      $(".troll").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
   });
+
+    $(".doge3").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge3").hide();
+      $(".troll").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+     $(".doge4").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge4").hide();
+      $(".troll").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+      $(".doge5").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge5").hide();
+      $(".troll").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge6").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge6").hide();
+      $(".troll").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge7").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge7").hide();
+      $(".troll").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge8").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge8").hide();
+      $(".troll").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+  var doge = '2', troll = '0';
+
+   $(".doge2").click(function() {
+
+    troll = '1';
+
+    if(doge==troll) {
+      $(".doge2").hide();
+      $(".troll2").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+    $(".doge3").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge3").hide();
+      $(".troll2").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+     $(".doge4").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge4").hide();
+      $(".troll2").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+      $(".doge5").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge5").hide();
+      $(".troll2").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge6").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge6").hide();
+      $(".troll2").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge7").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge7").hide();
+      $(".troll2").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge8").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge8").hide();
+      $(".troll2").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+
+  var doge = '2', troll = '0';
+
+     $(".doge3").click(function() {
+
+    troll = '1';
+
+    if(doge==troll) {
+      $(".doge3").hide();
+      $(".troll3").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+
+     $(".doge4").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge4").hide();
+      $(".troll3").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+      $(".doge5").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge5").hide();
+      $(".troll3").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge6").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge6").hide();
+      $(".troll3").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge7").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge7").hide();
+      $(".troll3").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge8").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge8").hide();
+      $(".troll3").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+  var doge = '2', troll = '0';
+
+     $(".doge4").click(function() {
+
+    troll = '1';
+
+    if(doge==troll) {
+      $(".doge4").hide();
+      $(".troll4").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+      $(".doge5").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge5").hide();
+      $(".troll4").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge6").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge6").hide();
+      $(".troll4").show();
+    }
+    else if(doge != troll) {
+
+    }
+
+    else {
+      $("body").html("<p>Cheating!</p>");
+    }
+    
+  });
+
+       $(".doge7").click(function() {
+
+    troll = '2';
+
+    if(doge==troll) {
+      $(".doge7").hide();
+      $(".troll4").show();
+    }
+//    else if(doge != troll) {
+
+ //   }
+
+ //   else {
+ //     $("body").html("<p>Cheating!</p>");
+ //   }
+    
+ //  });
+
+ //      $(".doge8").click(function() {
+
+ //   troll = '2';
+
+//    if(doge==troll) {
+ //     $(".doge8").hide();
+//      $(".troll4").show();
+//    }
+//    else if(doge != troll) {
+
+//    }
+
+//    else {
+ //     $("body").html("<p>Cheating!</p>");
+//    }
+    
+//  });
+
+
+  function doStuff() {
+  $("#gift").show();
+  }
+  setInterval(doStuff, 5000);
+
+  function stuff() {
+  $("#gift2").show();
+  }
+  setInterval(stuff, 15000);
 
   $(".crate").click(function() {
     $(".crate").hide();
+    $(".doge2").show();
+    $(".crate2").show();
+  });
+
+function morestuff() {
+  $("#gift3").show();
+  }
+  setInterval(morestuff, 25000);
+
+  $(".crate2").click(function() {
+    $(".crate2").hide();
+    $(".doge3").show();
+    $(".crate3").show();
+
+  });
+
+function ilikestuff() {
+  $("#gift4").show();
+  }
+  setInterval(ilikestuff, 35000);
+
+ $(".crate3").click(function() {
+    $(".crate3").hide();
+    $(".doge4").show();
+    $(".crate4").show();
+
+  });
+
+ function poop() {
+  $("#gift5").show();
+  }
+  setInterval(poop, 45000);
+
+ $(".crate4").click(function() {
+    $(".crate4").hide();
+    $(".doge5").show();
+    $(".crate5").show();
+
+    });
+
+ function lol() {
+  $("#gift6").show();
+  }
+  setInterval(lol, 55000);
+
+ $(".crate5").click(function() {
+    $(".crate5").hide();
+    $(".doge6").show();
+    $(".crate6").show();
+
+    });
+
+ function kawh() {
+  $("#gift7").show();
+  }
+  setInterval(kawh, 65000);
+
+ $(".crate6").click(function() {
+    $(".crate6").hide();
+    $(".doge7").show();
+    $(".crate7").show();
+
+    });
+
+ function me0w() {
+  $("#gift8").show();
+  }
+  setInterval(me0w, 75000);
+
+ $(".crate7").click(function() {
+    $(".crate7").hide();
+    $(".doge8").show();
+    $(".crate8").show();
+
+    });
+
+$(".crate8").click(function() {
+    $(".crate8").hide();
+
+    });
+
 
 });
-
- });
+});
